eb1cf8a4132362710d76a422da17fcf70d4c537c
harishkumarforever <harishking809@gmail.com>
2022-06-04 13:13:08 +0530

    Create localStorage.js

---
+++ localStorage.js
@@ -0,0 +1,28 @@
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();   
+
+localStorage.setItem(
+      "user",
+      JSON.stringify({ _id: "60f254ca148611bc22f8a4fa" })
+    );
